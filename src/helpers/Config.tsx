05563aa1ae0baad58798cd479b054e5f49f45891
J3alaban <149269049+J3alaban@users.noreply.github.com>
2026-01-12 19:38:25 +0300

    Update Config.tsx

--- src/helpers/Config.tsx
+++ src/helpers/Config.tsx
@@ -1,6 +1,6 @@
 export const Config ={
     api: {
-        baseUrl:"http://demirayhidrolik.com:8080" ,  //this url for the connecting backend api. is can chanced for remote api server.
-        frontend: { baseUrl: "http://localhost:3000"}
+        baseUrl:"http://demirayhidrolik.com:8080"   //this url for the connecting backend api. is can chanced for remote api server.
+     
     },
 } 
